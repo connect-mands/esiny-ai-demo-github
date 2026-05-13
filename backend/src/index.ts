451fb88b74828343772634c36f07676904689a2c
Mandeep Singh <connect.mands@gmail.com>
2026-05-13 17:51:38 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,37 @@
-import express, {Express} from "express";
-import cors from "cors"
+import express, { Express } from "express";
+import cors from "cors";
 import apiRouter from "./routes/index.js";
 import { PORT } from "./config/server.config.js";
 import dbConnect from "./config/dbConnect.js";
 
-const app:Express = express()
+const app: Express = express();
 
 const start = async (): Promise<void> => {
-    await dbConnect()
-    app.use(express.json())
-    app.use(express.urlencoded({extended:true}))
-    app.use(
-        cors({
-          origin: [
-            "http://localhost:5173",
-            "https://comfy-conkies-f0de1f.netlify.app",
-            "https://e8sowkkwoswg40wo44g44s80.snapphoundtests.com"
-          ],
-          credentials: true
-        })
-      )
-    app.use("/api", apiRouter)
+  await dbConnect();
 
-    app.listen(PORT,() => {
-        console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`)
+  // CORS FIRST
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "https://comfy-conkies-f0de1f.netlify.app",
+        "https://e8sowkkwoswg40wo44g44s80.snapphoundtests.com",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      credentials: true,
     })
-}
+  );
 
-start()
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use("/api", apiRouter);
+
+  app.listen(PORT, () => {
+    console.log(
+      `Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
+    );
+  });
+};
+
+start();
